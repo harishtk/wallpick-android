9ec6e9c702e1a5d95aa7486690555a1da1c9f94c
Haris <khariskumar@hitasoft.com>
2022-02-17 18:58:02 +0530

    UI Updates

--- app/src/main/java/com/harishtk/app/wallpick/MainActivity.kt
+++ app/src/main/java/com/harishtk/app/wallpick/MainActivity.kt
@@ -9,21 +9,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -67,20 +75,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(viewModel: MainViewModel) {
-    val responseState by viewModel.curatedPhotosResponse.observeAsState()
-
     val uiState = viewModel.uiState.collectAsState()
 
     Scaffold(
         topBar = {
             TopAppBar(
-                contentPadding = PaddingValues(start = 8.dp)
+                contentPadding = PaddingValues(start = 8.dp),
+                backgroundColor = Color.White,
+                elevation = 0.dp
             ) {
                 Row(
                     horizontalArrangement = Arrangement.SpaceAround,
                     modifier = Modifier.fillMaxWidth()
                 ) {
-                    Text(text = "Browse Wallpapers", style = MaterialTheme.typography.h1, fontSize = 22.sp, fontWeight = FontWeight.Bold, color = Color.White)
+                    Text(
+                        text = "Browse Wallpapers",
+                        style = MaterialTheme.typography.h1,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.Black
+                    )
                 }
             }
         }
@@ -92,6 +106,7 @@ fun Greeting(viewModel: MainViewModel) {
                 .fillMaxWidth()
         ) {
             Timber.d("$uiState")
+
             if (uiState.value.loading) {
                 CircularProgressIndicator() /* TODO: extract the indicator */
             } else if (uiState.value.error != null) {
@@ -122,49 +137,145 @@ fun Greeting(viewModel: MainViewModel) {
     }
 }
 
+@Composable
+fun SearchLayout(
+    uiState: State<UiState>? = null,
+    uiAction: ((UiAction) -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    var typedText by rememberSaveable { mutableStateOf("") }
+
+    Surface(
+        shape = RoundedCornerShape(25.dp),
+        color = Color.LightGray.copy(alpha = 0.5f),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .requiredHeight(40.dp),
+            contentAlignment = Alignment.CenterStart
+        ) {
+
+            if (typedText.isEmpty()) {
+                Text(
+                    text = "Search",
+                    style = MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.onSurface.copy(ContentAlpha.medium)),
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                BasicTextField(
+                    value = typedText,
+                    onValueChange = { typed: String ->
+                        typedText = typed
+                    },
+                    /*placeholder = "Type here!",*/
+                    singleLine = true,
+                    /*cursorColor = MaterialTheme.colors.primary.copy(alpha = 0.5f),*/
+                    modifier = Modifier.weight(1f)
+                        .padding(start = 16.dp)
+                )
+            }
+        }
+    }
+}
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PhotosList(photos: List<Photo>) {
-    LazyVerticalGrid(
+    /*LazyVerticalGrid(
         cells = GridCells.Adaptive(minSize = 128.dp)
     ) {
         items(photos) { photo ->
             PhotoItem(photo = photo)
             Spacer(modifier = Modifier.height(60.dp))
         }
-    }
-    /*LazyColumn(
-        contentPadding = PaddingValues(4.dp),
+    }*/
+    val lazyListState = rememberLazyListState()
+    var scrolledY = 0f
+    var previousOffset = 0
+
+    LazyColumn(
+        contentPadding = PaddingValues(
+            bottom = 16.dp
+        ),
+        state = lazyListState
     ) {
+        item {
+            SearchLayout(
+                modifier = Modifier
+                    .graphicsLayer {
+                        scrolledY += lazyListState.firstVisibleItemScrollOffset - previousOffset
+                        translationY = scrolledY * 0.5f
+                        previousOffset = lazyListState.firstVisibleItemScrollOffset
+                    }
+            )
+        }
+
         items(photos) { photo ->
             PhotoItem(photo = photo)
         }
-    }*/
+    }
 }
 
 @Composable
 fun PhotoItem(photo: Photo, modifier: Modifier = Modifier) {
     Card(
-        modifier = modifier.clickable {  }
+        modifier = modifier.clickable { }
     ) {
         Column {
-            Box {
-                Image(
-                    painter = rememberImagePainter(
-                        data = photo.src.small,
-                        builder = {
-                            transformations(RoundedCornersTransformation(0f))
-                            crossfade(true)
-                        },
-                    ),
-                    contentDescription = photo.alt,
-                    modifier = Modifier
-                        .height(128.dp),
-                    /*.aspectRatio(1F),*/
-                    contentScale = ContentScale.Crop,
-                    alignment = Alignment.TopCenter
+            Row {
+                Box(modifier = Modifier.weight(1F)) {
+                    Image(
+                        painter = rememberImagePainter(
+                            data = photo.src.large2x,
+                            builder = {
+                                transformations(RoundedCornersTransformation(0f))
+                                crossfade(true)
+                            },
+                        ),
+                        contentDescription = photo.alt,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1F),
+                        contentScale = ContentScale.Crop,
+                        alignment = Alignment.TopCenter
+                    )
+                }
+            }
+            /*Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                IconButton(onClick = { *//*TODO*//* },) {
+                    Icon(imageVector = Icons.Outlined.FavoriteBorder, contentDescription = "Favorite")
+                }
+                IconButton(onClick = { *//*TODO*//* }) {
+                    Icon(imageVector = Icons.Default.Share, contentDescription = "Options")
+                }
+            }
+            */
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+            ) {
+                Divider(modifier = Modifier.fillMaxWidth(0.5f), color = Color.LightGray)
+                Text(
+                    text = "~ ${photo.photographer}",
+                    style = MaterialTheme.typography.subtitle1.copy(color = Color.DarkGray, fontStyle = FontStyle.Italic),
+                    modifier = Modifier.padding(horizontal = 8.dp)
                 )
             }
+            Spacer(modifier = Modifier
+                .height(40.dp)
+                .fillMaxWidth())
         }
     }
 }
@@ -174,5 +285,6 @@ fun PhotoItem(photo: Photo, modifier: Modifier = Modifier) {
 fun DefaultPreview() {
     WallPickTheme {
         // Greeting("Android")
+        SearchLayout()
     }
 }
